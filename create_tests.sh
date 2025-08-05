24c1216534d9ed901849bc44376945270af12e67
João Novo <joaonovo05@protonmail.com>
2025-08-05 16:06:23 +0100

    added script for easier test creation

---
+++ create_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read -p "Enter a name: " name
+
+if [ -z "$name" ]; then
+    echo "Error: Name cannot be empty"
+    exit 1
+fi
+
+mkdir -p ./test
+
+dir_path="./test/$name"
+mkdir -p "$dir_path"
+
+touch "$dir_path/$name.mvs"
+touch "$dir_path/$name.out"
+cp ./config.toml "$dir_path/config.toml"
+
+echo "Successfully created:"
+echo "  Directory: $dir_path"
+echo "  Files: $name.mvs and $name.out"
